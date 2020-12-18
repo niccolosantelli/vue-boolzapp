96815b372d068905413db0633e9ce65ff02f15bd
niccolosantelli <niccolo_santelli@live.it>
2020-12-18 19:18:21 +0100

    completamento tasks contatti

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+
+let app = new Vue({
+  el:"#app",
+  data:{
+    contatti: [
+      {
+        nome: "Michele",
+        img:"assets/img/avatar_1.jpg",
+      },
+      {
+        nome: "Fabio",
+        img:"assets/img/avatar_2.jpg"
+      },
+      {
+        nome:"Samuele",
+        img:"assets/img/avatar_3.jpg"
+      },
+      {
+        nome:"Franco",
+        img:"assets/img/avatar_4.jpg"
+      },
+      {
+        nome:"Sara",
+        img:"assets/img/avatar_6.jpg"
+      },
+    ],
+  },
+
+
+
+
+})
